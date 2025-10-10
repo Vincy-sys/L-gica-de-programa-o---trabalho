d62e84744e2951129e3a543cd5ff87431fb99da3
Vincy D. D. Santos Andreata <vincy.andreatta@escola.pr.gov.br>
2025-10-10 11:50:39 -0300

    patch 2

--- trabalho logica da programação/app.js
+++ trabalho logica da programação/app.js
@@ -1,13 +1,30 @@
+let numeroSecreto = gerarNumero;
+let tentativas = 1;
 
-function exibirTexto(tag){
-let campo = document.querySelector(tag);
-campo.innerHTML = texto;
+function exibirTexto(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
 
 exibirTexto('h1', 'jogo do número secreto');
 exibirTexto('p', 'escolha um número ente 1 e 10');
 
 function verficarChute(){
-    console.log('botão clicado!');
+    let chute = document.querySelector('input').value;
+    if(chute == numeroSecreto){
+        exibirTexto ('h1', 'acertou!')
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : tentativa
+        let mensagemTentativas = `você descobriu o número secreto com ${tentativas} ${tentativas!}`;
+        exibirTexto('p', mensagemTentativas)
+    } else{
+        if (chute > numeroSecreto)
+        exibirTexto ('p', 'O número secreto é menor')
+     else{
+        exibirTexto('p', 'o número secreto é maior')
+        }
+        tentativas++;
+    }
 }
- a
+function gerarNumero(){
+    return parseInt(Math.random() * 10 + 1);
+}
